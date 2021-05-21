0f14b5a9210b7f30d8b21c07c1199467eb32c982
ka1ex <kalexadamson@hotmail.com>
2021-05-20 22:36:55 -0500

    commit tree 95b9ebdd1d3e553fe253a079917e98e8673cc2cb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(driversArray, driversName){
+
+let correctDriver = driversArray.filter(function(seltectedDriver){
+return seltectedDriver.toLowerCase() === driversName.toLowerCase()
+}) 
+return correctDriver
+}
+
+function fuzzyMatch (driverArr, driversName2){
+
+    let correctDriver2 = driverArr.filter((seltectedDriver2) => [...seltectedDriver2][0] === [...driversName2][0]
+    )
+    return correctDriver2
+}
+const matchName = (arrayOfDrivers, driversName3) =>{
+    let correctDriver3 = arrayOfDrivers.filter(chosenDriver => chosenDriver.name === driversName3)
+    return correctDriver3
+
+}
